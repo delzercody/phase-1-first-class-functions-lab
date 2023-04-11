011bb9d4066eecb8176f00477f8a4f3d1914a266
Cody Delzer <delzercody@gmail.com>
2023-04-10 22:20:09 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = () => {return drivers.slice(0, 2)};
+const returnLastTwoDrivers = () => {return drivers.slice(- 2)}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = (fare) => {return function(multiplier) {return fare * multiplier}}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = function selectDifferentDrivers(){
+    let selectedDrivers 
+    if(Math.random() > 0.5){
+        selectedDrivers = selectingDrivers[0]()
+    }else{
+        selectedDrivers = selectingDrivers[1]()
+    }
+    return selectedDrivers
+} 
